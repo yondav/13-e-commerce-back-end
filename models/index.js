81873ad8d94c4637d112d2adb1720bb03a7c536b
yondav <yonidavid.abr@gmail.com>
2021-04-26 14:39:55 -0400

    Model Associations complete. DB seeded successfully

--- models/index.js
+++ models/index.js
@@ -1,16 +1,37 @@
-// import models
+/**
+ * /models/index.js
+ *
+ * @description: Model Associations
+ *
+ */
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
