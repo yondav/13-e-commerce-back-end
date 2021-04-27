bef7b65f4914fa151b5b0c197f41714374d4235e
yondav <yonidavid.abr@gmail.com>
2021-04-26 20:12:09 -0400

    POST request routes work for all models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,8 +31,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
